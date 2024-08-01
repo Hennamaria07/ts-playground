7200116e16f8b2ff6479c773497f1e3368dc4153
Henna Maria <hennamaria2001@gmail.com>
2024-08-01 16:16:15 +0530

    array types added

---
+++ Array Types/generic.ts
@@ -0,0 +1,5 @@
+const items1: Array<string> = [];
+// const items: string[] = [];
+
+const items2: Array<number> = [];
+// const items2: number[] = [];
---
+++ Array Types/type.ts
@@ -0,0 +1,11 @@
+const nums: number[] = [1, 2, 3, 4, 5];
+console.log(nums);
+
+const str: string[] = ["one", "two", "three"];
+console.log(str);
+
+const items: string[] = [];
+items.push("Remote");
+items.push("Keyboard");
+// items.push(21); //throws error
+console.log(items);
