bfdd5effb21f73cbec8bbccb756af9602299e95d
Henna Maria <hennamaria2001@gmail.com>
2024-08-03 13:43:55 +0530

    enum and tuple type added

---
+++ tuples and enum/enum.ts
@@ -0,0 +1,19 @@
+enum WeatherConditions {
+    Sunny = "sunny",
+    Cloudy = "cloudy",
+    Rainy = "rainy",
+    Snowy = "snowy",
+  }
+  
+  enum Numbers {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+  }
+  const currentWeather = WeatherConditions.Sunny;
+  console.log(`The current weather is ${currentWeather}`);
+  console.log(WeatherConditions)
+  console.log(Numbers)
